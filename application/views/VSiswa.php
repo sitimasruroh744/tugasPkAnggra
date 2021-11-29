1e1a4cdf22810516d90c2399ee385b82d3fefb84
sitimasruroh744 <smasruroh744@gmail.com>
2021-11-29 12:07:42 +0700

    pengumpulan tugas

---
+++ application/views/VSiswa.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Siswa</title>
+</head>
+<body>
+
+<table border="1px">
+	<tr>
+		<td colspan="4">
+			<a href="<?php echo site_url('Welcome/VFormAddSiswa'); ?>">Add</a>
+		</td>
+	</tr>
+	<tr>
+		<th>NIS</th>
+		<th>Nama</th>
+		<th>No. TLP</th>
+		<th>Tools</th>
+	</tr>
+	<?php
+	if(!empty($DataSiswa))
+	{
+		foreach($DataSiswa as $ReadDS)
+		{
+	?>
+
+	<tr>
+		<td><?php echo $ReadDS->nis; ?></td>
+		<td><?php echo $ReadDS->nama; ?></td>
+		<td><?php echo $ReadDS->alamat; ?></td>
+		<td>
+			<a href="<?php echo site_url('Welcome/DataSiswa/'.$ReadDS->nis.'/view') ?>">Update</a>
+			<a href="<?php echo site_url('Welcome/DeleteDataSiswa/'.$ReadDS->nis) ?>">Delete</a>
+		</td>
+	</tr>
+
+	<?php		
+		}
+	}
+	?>
+</table>
+
+</body>
+</html>
